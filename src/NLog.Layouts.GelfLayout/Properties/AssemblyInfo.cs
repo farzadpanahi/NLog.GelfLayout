369ab02a7ef34d9cbe87ad0b4a83b8118d875fff
farzad <farzad.panahi@gmail.com>
2015-01-13 18:17:11 -0800

    updated version number

--- src/NLog.Layouts.GelfLayout/Properties/AssemblyInfo.cs
+++ src/NLog.Layouts.GelfLayout/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Farzad Panahi")]
 [assembly: AssemblyProduct("NLog.Layouts.GelfLayout")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.2.*")]
